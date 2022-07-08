26f76fded270175fa2b8e1be90c9c1d3422a7e29
Aman_Gora <aman.gora@orchids.edu.in>
2022-07-08 22:20:37 +0530

    Fields options

--- Person/models.py
+++ Person/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30,default='')
+    last_name = models.CharField(max_length=30,default='')
+    SHIRT_sIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=30,default='')
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_sIZES,default='')
+
 
 
     class Meta:
@@ -15,15 +23,15 @@ class Person(models.Model):
         return self.first_name + ' ' + self.last_name
 
 class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50,default='')
+    last_name = models.CharField(max_length=50,default='')
+    instrument = models.CharField(max_length=100,default='')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
 class Album(models.Model):      # Avoiding the use of the reserved word clean,save, or delete.
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_start = models.IntegerField()
+    name = models.CharField(max_length=100,default='')
+    release_date = models.DateField(default='')
+    num_start = models.IntegerField(default=0)
