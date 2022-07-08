f0c756faecbe6796389cb341dff73ff525a995f2
Aman_Gora <aman.gora@orchids.edu.in>
2022-07-08 22:08:10 +0530

    Fields

--- Person/models.py
+++ Person/models.py
@@ -8,8 +8,22 @@ class Person(models.Model):
 
 
     class Meta:
-        ordering = ['first_name']
-        db_table = '"person"'
+        ordering = ['first_name']                    # Python manage.py migrate
+        db_table = '"person"'                        # python manage.py makemigrations
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Album(models.Model):      # Avoiding the use of the reserved word clean,save, or delete.
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_start = models.IntegerField()
